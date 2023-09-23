40936f4e6246cfdf7becec98d7d8d45271f01ccc
lilingyun <glilingyun@gmail.com>
2023-09-23 11:00:02 +0800

    Update faker.sql

--- faker.sql
+++ faker.sql
@@ -1,8 +1,9 @@
 CREATE TABLE faker (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
     age INT,
-    gender VARCHAR(10),  -- 性别字段，可以是字符串类型
-    address TEXT          -- 地址字段，可以使用 TEXT 类型来存储较长的文本
+    phone_number VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    address TEXT,
+    gender VARCHAR(10)
 );
